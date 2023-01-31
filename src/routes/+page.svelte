19e5ccd9d1fa5fb2111fea863e73a9a5445673bd
QuentinJs1 <contact@quentinjs.com>
2023-01-31 21:54:56 +0100

    :tada: init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-lg">
+			<h1 class="text-5xl font-bold">Laissez-vous envelopper par le son.</h1>
+			<p class="py-6">
+				Écoutez plus de 100 millions de chansons et 30 000 playlists sans publicités. Plongez au
+				coeur du son grâce à l'audio spatial. Profitez de playlists uniques et de stations de radio
+				en direct.
+			</p>
+			<a href="/login">
+				<button class="btn btn-primary">1 mois d’essai gratuit</button>
+			</a>
+		</div>
+	</div>
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,26 @@
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+			<div class="card-body">
+				<div class="form-control">
+					<label for="email" class="label">
+						<span class="label-text">Email</span>
+					</label>
+					<input id="email" type="text" placeholder="email" class="input input-bordered" />
+				</div>
+				<div class="form-control">
+					<label for="password" class="label">
+						<span id="password" class="label-text">Password</span>
+					</label>
+					<input type="password" placeholder="password" class="input input-bordered" />
+					<label for="forgot" class="label">
+						<a id="forgot" href="/" class="label-text-alt link link-hover">Forgot password?</a>
+					</label>
+				</div>
+				<div class="form-control mt-6">
+					<button class="btn btn-primary">Login</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
